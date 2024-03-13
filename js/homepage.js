bfa20a7cf631d26d3037ab0fdb2031461104ce86
Jay <jordanbell321@gmail.com>
2024-03-13 19:14:09 +0000

    commit change

--- js/homepage.js
+++ js/homepage.js
@@ -5,7 +5,7 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (!event.target.matches('.btn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
@@ -17,18 +17,37 @@ window.onclick = function(event) {
         }
     }
 }
-  
-window.addEventListener('load', function() {
+
+window.addEventListener('load', function () {
     document.querySelectorAll('.squares').forEach(card => {
-      var cardPosition = card.getBoundingClientRect().top;
-      var screenPosition = window.innerHeight;
-      if (cardPosition < screenPosition) {
-        card.classList.add('visible');
-    }
+        var cardPosition = card.getBoundingClientRect().top;
+        var screenPosition = window.innerHeight;
+        if (cardPosition < screenPosition) {
+            card.classList.add('visible');
+        }
     });
 
 
-    
-}); 
 
-setTimeout(function(){confetti({ particleCount: 100, spread: 100, origin: { y: 0.6 } })}, 500);
+});
+
+setTimeout(function () { confetti({ particleCount: 100, spread: 100, origin: { y: 0.6 }, zIndex: 0 }) }, 500);
+
+
+// Search bar
+
+// JavaScript code
+
+function search_animal() {
+    let input = document.getElementById("searchbar").value;
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName("animals");
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        } else {
+            x[i].style.display = "list-item";
+        }
+    }
+}
