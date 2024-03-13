bf0818356e95dd62420073aad460b75fe5ec0edf
Jay <jordanbell321@gmail.com>
2024-03-13 17:25:54 +0000

    confetti :D

--- js/homepage.js
+++ js/homepage.js
@@ -1,9 +1,12 @@
+import confetti from "https://esm.run/canvas-confetti@1";
+
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
 window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.btn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
@@ -14,3 +17,18 @@ window.onclick = function(event) {
         }
     }
 }
+  
+window.addEventListener('load', function() {
+    document.querySelectorAll('.squares').forEach(card => {
+      var cardPosition = card.getBoundingClientRect().top;
+      var screenPosition = window.innerHeight;
+      if (cardPosition < screenPosition) {
+        card.classList.add('visible');
+    }
+    });
+
+
+    
+}); 
+
+setTimeout(function(){confetti({ particleCount: 100, spread: 100, origin: { y: 0.6 } })}, 500);
